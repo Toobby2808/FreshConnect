f0f5b6bc8aeb92e42c67fef4d2259d99084b152c
Adewale Adesanya <adesanyaadewale89@gmail.com>
2025-10-18 14:27:22 -0700

    New commits

--- src/firebase.js
+++ src/firebase.js
@@ -9,18 +9,12 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBkXFnud4e5_-QF8gNuQuXvFGv2l4Q2E5Q",
-  authDomain: "freshconnect-real-app.firebaseapp.com",
-  projectId: "freshconnect-real-app",
-  storageBucket: "freshconnect-real-app.firebasestorage.app",
-  messagingSenderId: "777483816209",
-  appId: "1:777483816209:web:06ea067d3ec5e323bb50b0",
-  /* apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID, */
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
